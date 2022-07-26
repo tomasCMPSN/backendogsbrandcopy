fe4ee1f72253f4ea5cd2e862dd92eb967cf4764c
Tomas Campusano <tomascampusano.10@gmail.com>
2022-07-26 18:20:43 -0300

    Added: morgan, cors and middlewares. Ready for routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import express from 'express'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 4000)
+app.set("port", process.env.PORT || 4000);
 
-app.listen(app.get('port'), () => {
-    console.log('using port '+ app.get('port'));
-})
+app.listen(app.get("port"), () => {
+  console.log("using port " + app.get("port"));
+});
 
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
