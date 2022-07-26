0caeacc83f4fc1d77824a1a14d51bf1247afe174
Tomas Campusano <tomascampusano.10@gmail.com>
2022-07-26 18:40:18 -0300

    Added: static index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 
@@ -14,3 +15,5 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname,'../public')));
