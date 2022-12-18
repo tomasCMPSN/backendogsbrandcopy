ae18858e1a74c2423b88d8dcf357d00622441eac
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-18 03:56:03 -0300

    Fixed: variable name

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
 
 const loginUser = async (req, res) => {
