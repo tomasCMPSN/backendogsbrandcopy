3decdcc35c28af2f2220493e6b81c7d0a0189e05
Tomas Campusano <tomascampusano.10@gmail.com>
2022-12-19 01:12:23 -0300

    Added: variable with mongo server

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+require('dotenv').config()
 
-const url = "mongodb://localhost:27017/ogsBrandCopy"
+// const url = "mongodb://localhost:27017/ogsBrandCopy"
+const url = process.env.REACT_APP_API_OGSBRANDCOPY
 
 mongoose.connect(url);
 
